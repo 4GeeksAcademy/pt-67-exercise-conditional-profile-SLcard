a174306ff91788a21d72fdad4051d251d196bbe7
Sonia <161872642+Inflanubes@users.noreply.github.com>
2024-05-13 19:16:07 +0000

    First commit

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,4 @@
+import { left, right } from "@popperjs/core";
 import "../style/index.css";
 
 /**
@@ -30,17 +31,16 @@ function render(variables = {}) {
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
 
   // reset the website body with the new html output
-  document.querySelector("#widget_content").innerHTML = `<div class="widget">
-            ${cover}
+  document.querySelector("#widget_content").innerHTML = `<div class="widget">${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+          <h1>  ${variables?.name ? variables.name : "Name"} ${variables?.lastName ? variables.lastName : "Lastname"}</h1>
+          <h2>${variables?.role ? variables.role : "Your role"}</h2>
+          <h3>${variables?.city ? variables.city : "City"}, ${variables?.country ? variables.country : "Country"}</h3>
+          <ul class= "${variables.socialMediaPosition}">
+            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter">${variables?.twitter ? variables.twitter : ""}</i></a></li>
+            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github">${variables?.github ? variables.github : ""}</i></a></li>
+            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin">${variables.linkedin ? variables.linkedin : ""}</i></a></li>
+            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram">${variables.instagram ? variables.instagram : ""}</i></a></li>
           </ul>
         </div>
     `;
